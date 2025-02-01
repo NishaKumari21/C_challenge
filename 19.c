b02bb5eaac4f37c46f02e29d878dd84089dfa999
Nisha Kumari <129987725+NishaKumari21@users.noreply.github.com>
2025-02-01 15:49:06 +0530

    Add files via upload

---
+++ 19.c
@@ -0,0 +1,10 @@
+// 9️⃣ Sum of digits of a number.
+#include<stdio.h>
+#include<conio.h>
+int main(){
+    int n;
+    printf("Enter a number: ");
+    scanf("%d",&n);
+    printf("%d\n",n*(n+1)/2);
+    return 0;
+}
